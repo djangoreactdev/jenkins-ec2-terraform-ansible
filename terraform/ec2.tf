453c652cdf29cb7f13ff250c300482a4a2546539
djangoreactdev <djangoreactdev@gmail.com>
2023-07-21 21:11:08 +0000

    Revert "add atttaching volume"
    This reverts commit 7db632637584a13a7ac743bdb678dd67edc5d389.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -29,78 +29,56 @@ resource "aws_instance" "server_jenkins" {
 
 }
 
-resource "null_resource" "configure_server" {
-  count = var.count_ec2_instance
-  triggers = {
-    trigger = aws_instance.server_jenkins[count.index].public_ip
-  }
+# resource "null_resource" "configure_server" {
+#   count = var.count_ec2_instance
+#   triggers = {
+#     trigger = aws_instance.server_jenkins[count.index].public_ip
+#   }
 
-  provisioner "local-exec" {
-    working_dir = "../ansible"
-    command     = "pwd && ansible-playbook --inventory ${aws_instance.server_jenkins[count.index].public_ip}, --private-key ${var.ssh_key_private} --user ec2-user deploy-docker-ec2-user.yaml"
-  }
-}
+#   provisioner "local-exec" {
+#     working_dir = "../ansible"
+#     command     = "pwd && ansible-playbook --inventory ${aws_instance.server_jenkins[count.index].public_ip}, --private-key ${var.ssh_key_private} --user ec2-user deploy-docker-ec2-user.yaml"
+#   }
+# }
 
 # ==================================== Add from snapshot ===============================================
 
-# resource "aws_volume_attachment" "jenkins_volume_attachment" {
-#   device_name = "/dev/xvdf"
-#   volume_id   = data.aws_ebs_volume.jenkins_volume.id
-#   instance_id = aws_instance.server_jenkins[0].id
-# }
+data "aws_ebs_volume" "prod_volume" {
+  most_recent = true
 
+  filter {
+    name   = "volume-id"
+    values = ["vol-026e1df5cf9f1ecea"]
+  }
+}
 
-# data "aws_ebs_volume" "jenkins_volume" {
-#   most_recent = true
+resource "aws_ebs_snapshot" "production_snapshot" {
+  volume_id = data.aws_ebs_volume.prod_volume.id
+}
 
-#   filter {
-#     name   = "volume-type"
-#     values = ["gp2"]
-#   }
+resource "aws_ebs_volume" "from_production_snapshot" {
+  availability_zone = var.availability_zone[0]
+  snapshot_id       = aws_ebs_snapshot.production_snapshot.id
+  size              = 8
+}
 
-#   filter {
-#     name   = "tag:Name"
-#     values = ["jenkins"]
-#   }
-# }
+resource "aws_instance" "non_production" {
+  count                       = var.count_ec2_instance
+  ami                         = data.aws_ami.amazon-linux-image.id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public_subnets[count.index].id
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  availability_zone           = var.availability_zone[count.index]
+  tags = {
+    Name   = "MyNew-Server"
+    Server = "MyNew-Server"
+  }
+}
 
-
-# data "aws_ebs_volume" "prod_volume" {
-#   most_recent = true
-
-#   filter {
-#     name   = "volume-id"
-#     values = ["vol-026e1df5cf9f1ecea"]
-#   }
-# }
-
-# resource "aws_ebs_snapshot" "production_snapshot" {
-#   volume_id = data.aws_ebs_volume.prod_volume.id
-# }
-
-# resource "aws_ebs_volume" "from_production_snapshot" {
-#   availability_zone = var.availability_zone[0]
-#   snapshot_id       = aws_ebs_snapshot.production_snapshot.id
-#   size              = 8
-# }
-
-# resource "aws_instance" "non_production" {
-#   count                       = var.count_ec2_instance
-#   ami                         = data.aws_ami.amazon-linux-image.id
-#   instance_type               = var.instance_type
-#   key_name                    = var.key_name
-#   associate_public_ip_address = true
-#   subnet_id                   = aws_subnet.public_subnets[count.index].id
-#   vpc_security_group_ids      = [aws_security_group.web.id]
-#   availability_zone           = var.availability_zone[count.index]
-#   tags = {
-#     Name   = "MyNew-Server"
-#     Server = "MyNew-Server"
-#   }
-# }
-
-# resource "aws_volume_attachment" "non_production" {
-#   device_name = "/dev/xvda"
-#   volume_id   = aws_ebs_volume.from_production_snapshot.id
-#   instance_id = aws_instance.non_production[0].id
-# }
+resource "aws_volume_attachment" "non_production" {
+  device_name = "/dev/xvda"
+  volume_id   = aws_ebs_volume.from_production_snapshot.id
+  instance_id = aws_instance.non_production[0].id
+}
