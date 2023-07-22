c17c169d569a18a76f61e85d1b39efb64a1bfece
djangoreactdev <djangoreactdev@gmail.com>
2023-07-22 22:44:06 +0000

    Untrack files in .gitignore

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -31,13 +31,13 @@ resource "aws_instance" "server_jenkins" {
 
 resource "null_resource" "configure_server" {
   count = var.count_ec2_instance
-  triggers = {
-    trigger = aws_instance.server_jenkins[count.index].public_ip
-  }
+  # triggers = {
+  #   trigger = aws_instance.server_jenkins[count.index].public_ip
+  # }
 
   provisioner "local-exec" {
     working_dir = "../ansible"
-    command     = "pwd && ansible-playbook --inventory ${aws_instance.server_jenkins[count.index].public_ip}, --private-key ${var.ssh_key_private} --user ec2-user deploy-docker-ec2-user.yaml"
+    command     = "pwd && ansible-playbook --inventory ${aws_instance.server_jenkins[count.index].public_ip}, --private-key ${var.ssh_key_private} --user ec2-user jenkins-to-ec2-to-s3-file-backup.yaml"
   }
 }
 
